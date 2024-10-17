660d98c1ad2d712cccb1b6f4077167f0856c64f7
Vikky-Lynx <vikkylynx@gmail.com>
2024-10-16 19:59:58 -0700

    finishTask403

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -20,7 +20,7 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 function findCommonElements(array1, array2) {
     let commonElements = [];
     for (let i = 0; i < array1.length; i++) {
-        if (includesElements(array2, array1[i])) {
+        if (includesElement(array2, array1[i])) {
             commonElements.push(array1[i]);
         }  
     }
